1f79f7ccbbcd51a951bd550d98be30c1f25f7de6
aeropriest <ashok.jaiswal@gmail.com>
2023-06-21 17:54:11 +0800

    * Added the gpt chain

--- pages/api/bot/index.js
+++ pages/api/bot/index.js
@@ -1,3 +1,4 @@
+import { getGptAnswer } from "@/lib/openai/";
 const TelegramBot = require("node-telegram-bot-api");
 
 async function handler(req, res) {
@@ -5,19 +6,23 @@ async function handler(req, res) {
   const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN);
   let ctx = req.body.callback_query || req.body.message;
   console.log(ctx);
-  if (!ctx) {
-    return res.status(200).json({ message: process.env.TELEGRAM_API_TOKEN });
+  if (ctx) {
+    try {
+      console.log(ctx, "My bot handler received");
+      // let message = `Hi 👋 ${ctx.from.first_name} I am SuitsBot! \n You can send me documents and I then ask questions about them.`;
+      const answer = await getGptAnswer(ctx.text);
+      bot.sendMessage(ctx.chat.id, answer);
+      return res.status(200).json({ name: "Hello Suites!!!" });
+    } catch (error) {
+      console.error(error.message);
+      bot.sendMessage(ctx.chat.id, "Error! " + error.message);
+      return res.status(200).json({ name: error.message });
+    }
   }
-  try {
-    console.log(ctx, "My bot handler received");
-    let message = `Hi 👋 ${ctx.from.first_name} I am SuitsBot! \n You can send me documents and I then ask questions about them.`;
-    bot.sendMessage(ctx.chat.id, message);
-    return res.status(200).json({ name: "Hello Suites!!!" });
-  } catch (error) {
-    console.error(error.message);
-    bot.sendMessage(ctx.chat.id, error.message);
-    return res.status(200).json({ name: error.message });
-  }
+  const { question } = req.query;
+  console.log(question);
+  const answer = await getGptAnswer(question);
+  return res.status(200).json({ question, answer });
 }
 
 export default handler;
