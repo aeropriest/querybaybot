4100aec3d9e9474ac07a442018310c6ff235b1a0
Joshua Lai <joshua@Joshuas-MacBook-Air.local>
2023-06-21 15:10:06 +0800

    * Added new bot api

--- pages/api/josh.js
+++ pages/api/josh.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const name = "Ashok";
 
 export default function handler(req, res) {
   console.log('requestarrived')
-  res.status(200).json({ name: 'My name John Doe' }) 
+  res.status(200).json({ name: name }) 
 }
